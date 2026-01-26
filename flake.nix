243276e61a7f729710802f2cbe41ddf20e4222f4
Alejandro Cabeza Romero <alex93cabeza@gmail.com>
2026-01-26 19:44:49 +0100

    wip

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.follows = "logos-liblogos/nixpkgs";
     logos-liblogos.url = "github:logos-co/logos-liblogos";
-    logos-cpp-sdk.url = "github:logos-co/logos-cpp-sdk";
+    logos-core.url = "github:logos-co/logos-cpp-sdk";
 
     logos-blockchain = {
       url = "github:logos-blockchain/logos-blockchain";
@@ -18,107 +18,74 @@
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system:
         let
           pkgs = import nixpkgs { inherit system; };
-          qt = pkgs.qt6;
           llvmPkgs = pkgs.llvmPackages;
-          logosSdk = inputs.logos-cpp-sdk.packages.${system}.default;
-          logosBlockchainSrc = inputs.logos-blockchain;
-        in
-        f { inherit pkgs qt llvmPkgs logosSdk logosBlockchainSrc; });
-    in
-    {
-      packages = forAllSystems ({ pkgs, qt, llvmPkgs, logosSdk, logosBlockchainSrc }:
-        {
-          default = pkgs.stdenv.mkDerivation {
-            pname = "logos-blockchain-module";
-            version = "dev";
-            src = ./.;
+          logosCore = inputs.logos-core.packages.${system}.default;
+          logosBlockchain = inputs.logos-blockchain;
+
+          env = {
+            LOGOS_CORE_ROOT = "${logosCore}";
+            LOGOS_BLOCKCHAIN_ROOT = "${logosBlockchain}";
+            LIBCLANG_PATH = "${llvmPkgs.libclang.lib}/lib";
+            CLANG_PATH = "${llvmPkgs.clang}/bin/clang";
+          };
+
+          args = {
+            inherit (env) LOGOS_CORE_ROOT LOGOS_BLOCKCHAIN_ROOT LIBCLANG_PATH CLANG_PATH;
 
             nativeBuildInputs = [
               pkgs.cmake
               pkgs.ninja
               pkgs.pkg-config
-              qt.wrapQtAppsHook
               pkgs.patchelf
             ];
 
             buildInputs = [
-              qt.qtbase
-              qt.qttools
-
+              pkgs.qt6.qtbase
+              pkgs.qt6.qttools
               pkgs.rustc
               pkgs.cargo
               pkgs.git
-
               llvmPkgs.clang
               llvmPkgs.llvm
               llvmPkgs.libclang
             ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
               pkgs.libiconv
             ];
+          };
+        in
+        f { inherit pkgs args; });
+    in
+    {
+      packages = forAllSystems ({ pkgs, args }:
+        {
+          default = pkgs.stdenv.mkDerivation (args // {
+            pname = "logos-blockchain-module";
+            version = "dev";
+            src = ./.;
 
-            LOGOS_CPP_SDK_ROOT = "${logosSdk}";
-            LOGOS_BLOCKCHAIN_ROOT = "${logosBlockchainSrc}";
-
-            LIBCLANG_PATH = "${llvmPkgs.libclang.lib}/lib";
-            CLANG_PATH = "${llvmPkgs.clang}/bin/clang";
+            nativeBuildInputs = args.nativeBuildInputs ++ [ pkgs.qt6.wrapQtAppsHook ];
 
             CARGO_HOME = "${"$"}TMPDIR/cargo-home";
-
-            cmakeFlags = [
-              "-DUNTITLED_USE_QT=ON"
-              "-DLOGOS_CPP_SDK_ROOT=${logosSdk}"
-              "-DLOGOS_BLOCKCHAIN_ROOT=${logosBlockchainSrc}"
-              "-DCOPY_PLUGIN_TO_SOURCE_DIR=OFF"
-            ];
 
             preConfigure = ''
               mkdir -p $CARGO_HOME
             '';
-          };
+          });
         }
       );
 
-      devShells = forAllSystems ({ pkgs, qt, llvmPkgs, logosSdk, logosBlockchainSrc }:
+      devShells = forAllSystems ({ pkgs, args }:
         {
-          default = pkgs.mkShell {
-            nativeBuildInputs = [
-              pkgs.cmake
-              pkgs.ninja
-              pkgs.pkg-config
-              pkgs.patchelf
-            ];
-
-            buildInputs = [
-              qt.qtbase
-              qt.qttools
-
-              pkgs.rustc
-              pkgs.cargo
-              pkgs.git
-
-              llvmPkgs.clang
-              llvmPkgs.llvm
-              llvmPkgs.libclang
-            ];
-
-            LOGOS_CPP_SDK_ROOT = "${logosSdk}";
-            LOGOS_BLOCKCHAIN_ROOT = "${logosBlockchainSrc}";
-
-            LIBCLANG_PATH = "${llvmPkgs.libclang.lib}/lib";
-            CLANG_PATH = "${llvmPkgs.clang}/bin/clang";
-
+          default = pkgs.mkShell (args // {
             shellHook = ''
               echo "Logos Blockchain Module dev environment"
-              echo "LOGOS_CPP_SDK_ROOT:    $LOGOS_CPP_SDK_ROOT"
+              echo "LOGOS_CORE_ROOT:       $LOGOS_CORE_ROOT"
               echo "LOGOS_BLOCKCHAIN_ROOT: $LOGOS_BLOCKCHAIN_ROOT"
-              echo "LIBCLANG_PATH:         $LIBCLANG_PATH"
-              echo "CLANG_PATH:            $CLANG_PATH"
               echo ""
               echo "Build with:"
-              echo "  just clean"
               echo "  just build"
             '';
-          };
+          });
         }
       );
     };
--- justfile
+++ justfile
@@ -1,34 +1,30 @@
 default: build
 
 configure:
-    test -n "${LOGOS_CPP_SDK_ROOT}" || (echo "LOGOS_CPP_SDK_ROOT not set" && exit 1)
-    test -n "${LOGOS_BLOCKCHAIN_ROOT}" || (echo "LOGOS_BLOCKCHAIN_ROOT not set" && exit 1)
     cmake -S . -B build -G Ninja \
-      -DLOGOS_CPP_SDK_ROOT="${LOGOS_CPP_SDK_ROOT}" \
-      -DLOGOS_BLOCKCHAIN_ROOT="${LOGOS_BLOCKCHAIN_ROOT}" \
+      ${LOGOS_CORE_ROOT:+-DLOGOS_CORE_ROOT="$LOGOS_CORE_ROOT"} \
+      ${LOGOS_BLOCKCHAIN_ROOT:+-DLOGOS_BLOCKCHAIN_ROOT="$LOGOS_BLOCKCHAIN_ROOT"} \
       -DCOPY_PLUGIN_TO_SOURCE_DIR=ON
 
 build:
     cmake --build build --parallel --target liblogos-blockchain-module
 
 update: clean-full
-    cmake --build build --parallel --target logos_blockchain_stage
     cmake --build build --parallel --target logos_cargo_build
     just build
 
 clean:
-    rm -f build/liblogos-blockchain-module.so
-    rm -f liblogos-blockchain-module.so
+    rm -rf build
+    rm -rf target
     rm -f liblogos-blockchain-module.log
     rm -rf logos_blockchain_db/
 
 clean-full: clean
-    rm -rf build
 
 rebuild: clean configure build
 
 run:
-    RUST_BACKTRACE=full RUST_LOG=trace ../logos-module-viewer/result/bin/logos-module-viewer --module liblogos-blockchain-module.so > liblogos-blockchain-module.log 2>&1
+    RUST_BACKTRACE=full RUST_LOG=trace ../logos-module-viewer/result/bin/logos-module-viewer --module target/liblogos-blockchain-module.so > liblogos-blockchain-module.log 2>&1
 
 nix:
     nix develop .#
