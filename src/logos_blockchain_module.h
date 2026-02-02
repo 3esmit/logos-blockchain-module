4d03f0919c04e4c6608e102ad8c548508067e24f
Alejandro Cabeza Romero <alex93cabeza@gmail.com>
2026-02-02 17:28:11 +0100

    Add default behaviour to config_path and handle deployment in start.

--- src/logos_blockchain_module.h
+++ src/logos_blockchain_module.h
@@ -2,23 +2,29 @@
 
 #include "i_logos_blockchain_module.h"
 
-class LogosBlockchainModule final : public ILogosBlockchainModule {
+class LogosBlockchainModule final : public QObject, public PluginInterface, public ILogosBlockchainModule {
     Q_OBJECT
     Q_PLUGIN_METADATA(IID ILogosBlockchainModule_iid FILE "../metadata.json")
+    Q_INTERFACES(PluginInterface)
 
 public:
     LogosBlockchainModule();
     ~LogosBlockchainModule() override;
 
+    // Logos Core
     [[nodiscard]] QString name() const override;
     [[nodiscard]] QString version() const override;
-
     Q_INVOKABLE void initLogos(LogosAPI*) override;
-    Q_INVOKABLE int start(const QString&) override;
+
+    // Logos Blockchain
+    Q_INVOKABLE int start(const QString& config_path, const QString& deployment) override;
     Q_INVOKABLE int stop() override;
     Q_INVOKABLE int subscribe() override;
     Q_INVOKABLE int wallet_get_balance(const uint8_t*, const HeaderId*, BalanceResult*) override;
     Q_INVOKABLE int wallet_transfer_funds(const TransferFundsArguments*, Hash*) override;
+
+    signals:
+        void eventResponse(const QString& eventName, const QVariantList& data);
 
 private:
     LogosBlockchainNode* node = nullptr;
