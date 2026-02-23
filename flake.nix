a1463a95acc6181555222219b2d328f9f738bd09
Alejandro Cabeza Romero <alex93cabeza@gmail.com>
2026-02-23 15:04:22 +0100

    Move logos-blockchain ref to master.

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     logos-liblogos.url = "github:logos-co/logos-liblogos";
     logos-core.url = "github:logos-co/logos-cpp-sdk";
 
-    logos-blockchain.url = "github:logos-blockchain/logos-blockchain?ref=4945402";
+    logos-blockchain.url = "github:logos-blockchain/logos-blockchain";
 
     logos-module-viewer.url = "github:logos-co/logos-module-viewer";
   };
