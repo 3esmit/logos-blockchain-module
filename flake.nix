888a66849ffc9babeb33ac070145b29861f0a23d
Alejandro Cabeza Romero <alex93cabeza@gmail.com>
2026-02-24 11:56:41 +0100

    Update blockchain to 0.1.7.

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     logos-liblogos.url = "github:logos-co/logos-liblogos";
     logos-core.url = "github:logos-co/logos-cpp-sdk";
 
-    logos-blockchain.url = "github:logos-blockchain/logos-blockchain?ref=0.1.6";
+    logos-blockchain.url = "github:logos-blockchain/logos-blockchain?ref=0.1.7";
 
     logos-module-viewer.url = "github:logos-co/logos-module-viewer";
   };
