08bd849d5a4d331d7c206cdd291105c95147dc12
Alejandro Cabeza Romero <alex93cabeza@gmail.com>
2026-04-29 18:38:08 +0200

    Update lb dependency, potential note id issue fix.

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     logos-liblogos.url = "github:logos-co/logos-liblogos";
     logos-core.url = "github:logos-co/logos-cpp-sdk";
 
-    logos-blockchain.url = "github:logos-blockchain/logos-blockchain?rev=e71c4150ba2d01f61cade4b06ab54ce42756e17c"; # pre-0.1.3 + explicit handle parameter
+    logos-blockchain.url = "github:logos-blockchain/logos-blockchain?rev=88941ff33f2e028591b9d0ed2549a328d54f0cfa"; # pre-0.1.3 + potential note fixes
 
     logos-module-viewer.url = "github:logos-co/logos-module-viewer";
   };
