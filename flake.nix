f6ac0017245e18a9cba65a00bbffc6ec52536fb7
Ricardo Guilherme Schmidt <3esmit@gmail.com>
2026-07-23 04:26:44 -0300

    fix: pin resilient Testnet core

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     logos-module-builder.url = "github:logos-co/logos-module-builder?ref=38ddf92c1f240f4e420d300a1fbabb1609d5db01";
-    logos-blockchain.url = "github:3esmit/logos-blockchain?rev=3c400456fce46efc04fd5768facb6e13af1803f3";
+    logos-blockchain.url = "github:3esmit/logos-blockchain?rev=ee9083656e589b39ff8544cb4d92b1a8caa1010c";
   };
 
   outputs = inputs@{ logos-module-builder, ... }:
