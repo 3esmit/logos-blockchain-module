60ebcb5f039b56bba5220c5517c9a0029a5850e5
Ricardo Guilherme Schmidt <3esmit@gmail.com>
2026-08-09 00:53:04 -0300

    fix(deps): pin merged blockchain fork

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     logos-module-builder.url = "github:3esmit/logos-module-builder?rev=324b459c3f7b59171d249f3ccbcc362403b3fcaf";
-    logos-blockchain.url = "github:3esmit/logos-blockchain?rev=4f4e3b93e7f4187d8d5aa45f654cd5b48e6ac2b6";
+    logos-blockchain.url = "github:3esmit/logos-blockchain?rev=7befc1e26efdd46a30b58ef92e05cb5922c7038a";
   };
 
   outputs = inputs@{ logos-module-builder, ... }:
