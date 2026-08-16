4a97f524bc2fb70d39cd381400bbe46e495ee137
Ricardo Guilherme Schmidt <3esmit@gmail.com>
2026-08-16 12:19:34 -0300

    fix(testnet): consume foreign-genesis IBD guard

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     logos-module-builder.url = "github:3esmit/logos-module-builder?rev=324b459c3f7b59171d249f3ccbcc362403b3fcaf";
-    logos-blockchain.url = "github:3esmit/logos-blockchain?rev=759d2e2e41ba118b6a3ebbdf8617007e4bb050b2";
+    logos-blockchain.url = "github:3esmit/logos-blockchain?rev=32675a4a020c6ae328fb3280aca7c770633147ed";
   };
 
   outputs = inputs@{ logos-module-builder, ... }:
