3fd27d4502c87dad0bbf04c52eb66d42a89b1509
Ricardo Guilherme Schmidt <3esmit@gmail.com>
2026-08-15 22:47:04 -0300

    fix(testnet): use IBD cleanup-safe blockchain source

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     logos-module-builder.url = "github:3esmit/logos-module-builder?rev=324b459c3f7b59171d249f3ccbcc362403b3fcaf";
-    logos-blockchain.url = "github:3esmit/logos-blockchain?rev=87487951b4e33ec4e6134153da3d77cc048e305e";
+    logos-blockchain.url = "github:3esmit/logos-blockchain?rev=4f97d3861662d0c45a52d6801d7a7b7e2ca0f1fc";
   };
 
   outputs = inputs@{ logos-module-builder, ... }:
