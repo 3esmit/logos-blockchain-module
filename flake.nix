66e6d63544d5109b48b1af3b7f8b1d3c8b5d53e7
Ricardo Guilherme Schmidt <3esmit@gmail.com>
2026-08-17 07:04:30 -0300

    fix(testnet): use connected-peer bootstrap fix

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     logos-module-builder.url = "github:3esmit/logos-module-builder?rev=324b459c3f7b59171d249f3ccbcc362403b3fcaf";
-    logos-blockchain.url = "github:3esmit/logos-blockchain?rev=51cf0529cb9b60ae8a2cf5373522c2b9c5374998";
+    logos-blockchain.url = "github:3esmit/logos-blockchain?rev=b247385e4d501e343043b3afcfaa617d8fef7851";
   };
 
   outputs = inputs@{ logos-module-builder, ... }:
