1b4ea219ce71c431ad271d2d89d7e2a058856880
Ricardo Guilherme Schmidt <3esmit@gmail.com>
2026-08-17 08:02:32 -0300

    fix(testnet): combine deployed profile with connected-peer IBD

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     logos-module-builder.url = "github:3esmit/logos-module-builder?rev=324b459c3f7b59171d249f3ccbcc362403b3fcaf";
-    logos-blockchain.url = "github:3esmit/logos-blockchain?rev=759d2e2e41ba118b6a3ebbdf8617007e4bb050b2";
+    logos-blockchain.url = "github:3esmit/logos-blockchain?rev=c6da59d5d870c062d8ea14932b2c94499ff12097";
   };
 
   outputs = inputs@{ logos-module-builder, ... }:
